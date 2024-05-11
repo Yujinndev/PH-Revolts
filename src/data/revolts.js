f4533973def5b2237e4cbd5551d5ae402e19d988
Yujinndev <xyujinwho@gmail.com>
2024-05-12 00:57:24 +0800

    feat: add data

---
+++ src/data/revolts.js
@@ -0,0 +1,323 @@
+export const REVOLTS = [
+  {
+    href: '#manila',
+    icon: 1,
+    title: 'Lakandula and Sulayman Revolt (1574)',
+    place: 'Tondo, Navotas (Manila)',
+    leader: 'Rajah Lakandula and Rajah Sulayman',
+    cause:
+      'Failure of Gov. Lavezares to fulfill Legazpi’s promise to Lakandula.',
+    result:
+      'It failed because the revolt was suppressed after the intervention of Juan de Salcedo and Father Geronimo Martin and a promise of exemption to them.',
+    geocodes: [14.67149, 120.93985],
+    image: null,
+  },
+  {
+    href: '#pampanga',
+    icon: 1,
+    title: 'Pampanga Revolt (1585)',
+    place: 'Pampanga',
+    leader: 'Kapampangan Leaders',
+    cause:
+      'Disgruntled by the way the encomenderos administered who had deprived them of their historical inheritances as tribal chiefs or Datus.',
+    result:
+      'The revolt failed. A Filipina betrayed the revolt. She is married to a Spanish soldier who reported the plot to Spanish authorities.',
+    geocodes: [15.02393, 120.655243],
+    image: null,
+  },
+  {
+    href: '#maharlikas',
+    title: 'Conspiracy of the Maharlikas (1587 - 1588)',
+    place: 'Tondo (Manila), Cuyo (Palawan), Calamianes (Calamian Islands)',
+    leader:
+      'Don Agustín de Legazpi, Magat Salamat, Martin Pangan, Juan Banal, and Pedro Balingit',
+    cause:
+      'Continuous injustice, lack of respect for treaty obligations, and desire for independence.',
+    result:
+      'It failed because the plot was discovered by another Cuyo native whose master is a Spanish encomendero. The leaders were hanged, and their heads cut off and exposed on the gibbet in iron cages and the others were exiled to Mexico.',
+    geocodes: [14.6189095, 120.9660386],
+    image: null,
+  },
+  {
+    href: '#tribute',
+    icon: 1,
+    title: 'Revolts Against the Tribute (1589)',
+    place: 'Cagayan, Ilocos Norte and Ilocos Sur',
+    leader: '',
+    cause: 'Refusal to pay tributes, tyranny of tribute collectors',
+    result: 'The revolt failed and they were easily suppressed.',
+    geocodes: [18, 121.833333],
+    image: null,
+  },
+  {
+    href: '#magalat',
+    icon: 1,
+
+    title: 'Magalat Revolt (1596)',
+    place: 'Cagayan',
+    leader: 'Magalat (w/ brother)',
+    cause:
+      'Resentment of the natives of the payment of tribute and the implementation of the polo y servicio.',
+    result:
+      'It failed because the leader was assassinated and it was the first in Philippine history. He was not killed by the Spanish but by his men in his headquarters after they had been promised a reward from the Spaniards.',
+    geocodes: [18, 121.833333],
+    image: null,
+  },
+  {
+    href: '#igorot',
+    icon: 1,
+    title: 'Igorot Revolt (1601)',
+    place: 'Northern Luzon',
+    leader: 'Magalat (w/ brother)',
+    cause:
+      'Resentment of the natives of the payment of tribute and the implementation of the polo y servicio.',
+    result:
+      'It failed because the leader was assassinated and it was the first in Philippine history. He was not killed by the Spanish but by his men in his headquarters after they had been promised a reward from the Spaniards.',
+    geocodes: [18.195691785791198, 121.21708920884063],
+    image: null,
+  },
+  {
+    title: 'Irrayas (1621)',
+    place: 'Cagayan Valley',
+    leader: 'Felipe Cutabay and Gabriel Dayag',
+    cause: 'Oppression of the Indios by the Spanish officials.',
+    result: 'It was not materialized due to Fr. Pedro Sto. Tomas preaching.',
+    geocodes: [17.6739801, 121.8847642],
+    image: null,
+  },
+  {
+    title: 'Itneg Revolt or Mandaya Revolt (1625 - 1627)',
+    place: 'Northwestern Cagayan (Apayao)',
+    leader: 'Miguel Lanab and Aldaban',
+    cause:
+      'They desire independence and also the punishment of a woman who displeases certain Spanish officials.',
+    result:
+      'It failed and the leaders were pardoned and then later killed when they revolted anew.',
+    geocodes: [18.12, 121.19],
+    image: null,
+  },
+  {
+    title: 'Cagayan Revolt (1639)',
+    place: 'Northwestern Cagayan (Apayao)',
+    leader: '',
+    cause: 'Dissatisfaction with Spanish rule.',
+    result: 'The revolt was suppressed.',
+    geocodes: [18, 121.833333],
+    image: null,
+  },
+  {
+    title: 'Ladia Revolt (1643)',
+    place: 'Malolos, Bulacan and Sotuhern Luzon',
+    leader: 'Pedro Ladia',
+    cause: 'Weariness from Spanish oppression',
+    result: 'The revolt ended in failure and the leader was executed.',
+    geocodes: [14.843759, 120.8113694],
+    image: null,
+  },
+  {
+    title: 'Ladia Revolt (1643)',
+    place: 'Malolos, Bulacan and Sotuhern Luzon',
+    leader: 'Pedro Ladia',
+    cause: 'Weariness from Spanish oppression',
+    result: 'The revolt ended in failure and the leader was executed.',
+    geocodes: [14.843759, 120.8113694],
+    image: null,
+  },
+  {
+    title: 'Maniago Revolt (1660 - 1661)',
+    place: 'Bacoor, Cavite and Pampanga',
+    leader: 'Francisco Maniago',
+    cause:
+      'Failure of officials to pay rice purchased and repeated requirements for polo y servicio.',
+    result:
+      'General amnesty granted to rebels and initial payment for services.',
+    geocodes: [14.458816, 120.959579],
+    image: null,
+  },
+  {
+    title: 'Malong Revolt (1660)',
+    place: 'Binalatongan, Pangasinan',
+    leader: 'Andres Malong and Pedro Gumpaos',
+    cause:
+      'The revolt was initially caused by native’s protest against the polo and bandala.',
+    result:
+      'The revolt ended in failure. The leader was tricked into leaving Manila and was shot months later in Mexico, Pampanga.',
+    geocodes: [15.9218655, 120.3516846],
+    image: null,
+  },
+  {
+    title: 'Almazan Revolt (1661)',
+    place: 'San Nicolas, Baccara, Laog (Ilocos Norte)',
+    leader: 'Juan Magsanop, Pedro Almazan and Gaspar Cristobal',
+    cause: 'Severe injustice of the Spaniards',
+    result: 'It failed and the leaders were killed.',
+    geocodes: [18.174906, 120.5943667],
+    image: null,
+  },
+  {
+    title: 'Almazan Revolt (1661)',
+    place: 'San Nicolas, Baccara, Laog (Ilocos Norte)',
+    leader: 'Juan Magsanop, Pedro Almazan and Gaspar Cristobal',
+    cause: 'Severe injustice of the Spaniards',
+    result: 'It failed and the leaders were killed.',
+    geocodes: [18.2517377, 120.6106589],
+    image: null,
+  },
+  {
+    title: 'Almazan Revolt (1661)',
+    place: 'San Nicolas, Baccara, Laog (Ilocos Norte)',
+    leader: 'Juan Magsanop, Pedro Almazan and Gaspar Cristobal',
+    cause: 'Severe injustice of the Spaniards',
+    result: 'It failed and the leaders were killed.',
+    geocodes: [18.1997546, 120.5850928],
+    image: null,
+  },
+  {
+    title: 'Agrarian Revolt (1745 - 1746)',
+    place: 'Bulacan, Morong (Rizal)',
+    leader: 'Francisco Matienza',
+    cause: 'Usurpation of Filipino lands by religious orders.',
+    result: 'The revolt failed.',
+    geocodes: [15, 121.083333],
+    image: null,
+  },
+  {
+    title: 'Agrarian Revolt (1745 - 1746)',
+    place: 'Cavite',
+    leader: 'Francisco Matienza',
+    cause: 'Usurpation of Filipino lands by religious orders.',
+    result: 'The revolt failed.',
+    geocodes: [14.5151374, 121.2380858],
+    image: null,
+  },
+  {
+    title: 'Agrarian Revolt (1745 - 1746)',
+    place: 'Cavite, Laguna',
+    leader: 'Francisco Matienza',
+    cause: 'Usurpation of Filipino lands by religious orders.',
+    result: 'The revolt failed.',
+    geocodes: [14.342502, 121.0676242],
+    image: null,
+  },
+  {
+    title: 'Silang Revolt (1762 - 1763)',
+    place: 'Ilocos',
+    leader: 'Diego Silang and Gabriela Silang',
+    cause:
+      'Desire to expel the Spaniards from Ilocos. Unreasonable taxes and free labor of constructions.',
+    result:
+      'The revolt failed because of the traitorous act of his friends Miguel Vicos and Pedro Becbec that was instigated by the Spanish church and government authorities. His wife was captured and put to death publicly by hanging in September 1763.',
+    geocodes: [17.2, 120.5],
+    image: null,
+  },
+  {
+    title: 'Palaris Revolt (1762 - 1765)',
+    place: 'Pangasinan',
+    leader: 'Juan Dela Cruz Palaris',
+    cause: 'Demand for reforms especially the changing of local officials.',
+    result: 'It failed and Palaris was executed.',
+    geocodes: [15.916667, 120.333333],
+    image: null,
+  },
+  {
+    title: 'Basi Revolt (1807)',
+    place: 'Ilocos',
+    leader: 'Pedro Mateo',
+    cause: 'Government monopoly of basi manufacturing.',
+    result: 'It failed and the rebels were crushed by government troops.',
+    geocodes: [18.166667, 120.75],
+    image: null,
+  },
+  {
+    title: 'Hermano Pule (1840 - 1841)',
+    place: 'Quezon Province',
+    leader: 'Apolinario Dela Cruz (Hermano Pule)',
+    cause:
+      'Being a native, Pule was denied to be admitted as a monk. He founded the religious brotherhood, Confradia De San Jose, which the government outlawed and ordered to be dissolved.',
+    result:
+      'The revolt ended in failure and the leader was executed in Tayabas Quezon.',
+    geocodes: [13.9, 122],
+    image: null,
+  },
+  {
+    title: 'Tamblot Revolt (1621 - 1622)',
+    place: 'Bohol',
+    leader: 'Babaylan Tamblot',
+    cause: 'Desire to abandon Christianity and return to old religious faith.',
+    result:
+      'It ended in failure because they were routed by the combined forces of Spanish soldiers and Cebuanos.',
+    geocodes: [9.833333, 124.1615579],
+    image: null,
+  },
+  {
+    title: 'Bankaw Revolt (1621 - 1622)',
+    place: 'Leyte',
+    leader: 'Bankaw Pagali',
+    cause: 'They wanted to go back to their old tradition which is animism',
+    result:
+      'It failed because they were routed by the combined forces of Spanish soldiers and Cebuanos.',
+    geocodes: [10.7841157, 124.892321],
+    image: null,
+  },
+  {
+    title: 'Sumuroy Revolt (1644)',
+    place: 'Samar',
+    leader: 'Francisco Sumuroy',
+    cause:
+      'Refusal to send able-bodied men to cut timber and make galleon ships in Cavite.',
+    result:
+      'Sumuroy won several battles and repulsed several expeditions. His capture led to the failure of the revolt.',
+    geocodes: [11.833333, 125],
+    image: null,
+  },
+  {
+    title: 'Visayan Revolt (1649 - 1650)',
+    place: 'Eastern Visayas, Northern Mindanao, Zamboanga',
+    leader: 'Juan Ponce Sumoroy and Pedro Caamug',
+    cause:
+      'Caused by Gov. Fajardo’s order to send Visayan laborers to Cavite for shipbuilding.',
+    result: 'The leaders were captured and were beheaded.',
+    geocodes: [11.2885518, 124.9991536],
+    image: null,
+  },
+  {
+    title: 'Panay Revolt (1663)',
+    place: 'Otton, Panay',
+    leader: 'Tapar',
+    cause: 'They desire to put up a modified form of Christianity',
+    result: 'The revolt was suppressed.',
+    geocodes: [11.1728123, 122.5557688],
+    image: null,
+  },
+  {
+    title: 'Dagohoy Revolt (1744 - 1828)',
+    place: 'Bohol',
+    leader: 'Francisco Dagohoy',
+    cause:
+      'Refusal of Fr. Morales to give Dagohoy’s brother a Christian burial',
+    result:
+      'The revolt failed but it lasted for 85 years. Dagohoy died without seeing the end of his clamor and the rebels were pardoned.',
+    geocodes: [9.833333, 124.1615579],
+    image: null,
+  },
+  {
+    title: 'Moro Wars (1578)',
+    place: 'Jolo',
+    leader: '',
+    cause: 'Conversions of Muslim to Catholicism',
+    result:
+      'The Muslims of Mindanao conducted a series of raids to Visayas and Luzon. They burned the churches, took the inhabitants’ treasure, and even took some captives to become slaves.',
+    geocodes: [6.0536561, 121.0002644],
+    image: null,
+  },
+  {
+    title: 'Caraga Revolt (1629 - 1631)',
+    place: 'Caraga, Northern Mindanao',
+    leader: 'Mangabon',
+    cause: 'Dissatisfaction of townspeople to Spanish rule.',
+    result:
+      'The revolt failed and many of the rebels surrendered to the Spaniards the guilty ones were punished and killed and the others were made slaves.',
+    geocodes: [9.2471392, 125.8539592],
+    image: null,
+  },
+]
